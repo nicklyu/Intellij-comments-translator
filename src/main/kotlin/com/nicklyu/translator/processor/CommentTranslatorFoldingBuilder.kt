3b13b5b51d6ec1508a727b5af5fcabbe908ae606
nickly <lyubimov.nkt@gmail.com>
2018-09-06 03:08:13 +0300

    Single line comments highlighted. Code refactored.

--- src/main/kotlin/com/nicklyu/translator/processor/CommentTranslatorFoldingBuilder.kt
+++ src/main/kotlin/com/nicklyu/translator/processor/CommentTranslatorFoldingBuilder.kt
@@ -9,12 +9,16 @@ import com.intellij.openapi.editor.FoldingGroup
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.JavaDocTokenType.DOC_COMMENT_DATA
 import com.intellij.psi.JavaDocTokenType.DOC_COMMENT_LEADING_ASTERISKS
+import com.intellij.psi.JavaTokenType.END_OF_LINE_COMMENT
+import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
 import com.intellij.psi.javadoc.PsiDocComment
 import com.intellij.psi.javadoc.PsiDocToken
 import com.intellij.psi.util.PsiTreeUtil
 
 class CommentTranslatorFoldingBuilder : FoldingBuilderEx() {
+    private val foldingGroup = FoldingGroup.newGroup("CommentTranslatorFolding")
+
     override fun getPlaceholderText(node: ASTNode, range: TextRange): String {
         return ". . ."
     }
@@ -24,11 +28,28 @@ class CommentTranslatorFoldingBuilder : FoldingBuilderEx() {
     }
 
     override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
-        val foldingGroup = FoldingGroup.newGroup("CommentTranslatorFolding")
         val descriptors = mutableListOf<FoldingDescriptor>()
 
+        descriptors.addAll(processJavadocComments(root))
+        descriptors.addAll(processSingleLineComments(root))
+
+        return descriptors.toTypedArray()
+    }
+
+    private fun processJavadocComments(analyzedElement: PsiElement): Array<FoldingDescriptor> {
+
+        fun areElementsRepresentsJavadocLine(first: PsiElement, second: PsiElement?): Boolean =
+                first is PsiDocToken &&
+                        first.tokenType == DOC_COMMENT_LEADING_ASTERISKS &&
+                        second != null &&
+                        second is PsiDocToken &&
+                        second.tokenType == DOC_COMMENT_DATA &&
+                        second.text.trim().isNotEmpty()
+
+
+        val descriptors = mutableListOf<FoldingDescriptor>()
         val javadocComments = mutableMapOf<PsiElement, PsiElement>()
-        PsiTreeUtil.findChildrenOfType(root, PsiDocComment::class.java)
+        PsiTreeUtil.findChildrenOfType(analyzedElement, PsiDocComment::class.java)
                 .forEach { comment ->
 
                     val commentParts = comment.children
@@ -39,27 +60,37 @@ class CommentTranslatorFoldingBuilder : FoldingBuilderEx() {
                     }
                 }
 
-        javadocComments.forEach { asterisks, comment ->
+        javadocComments.forEach { asterisk, comment ->
             descriptors.add(
                     NamedFoldingDescriptor(
-                            asterisks.node,
+                            asterisk.node,
                             TextRange(comment.textRange.startOffset, comment.textRange.endOffset),
                             foldingGroup,
-                            comment.text
+                            "${comment.text} highlighted"
                     )
             )
         }
-
         return descriptors.toTypedArray()
     }
 
-    private fun areElementsRepresentsJavadocLine(first: PsiElement, second: PsiElement?): Boolean =
-            first is PsiDocToken &&
-                    first.tokenType == DOC_COMMENT_LEADING_ASTERISKS &&
-                    second != null &&
-                    second is PsiDocToken &&
-                    second.tokenType == DOC_COMMENT_DATA &&
-                    second.text.trim().isNotEmpty()
+    private fun processSingleLineComments(analyzedElement: PsiElement): Array<FoldingDescriptor> {
+        val descriptors = mutableListOf<FoldingDescriptor>()
+        PsiTreeUtil.findChildrenOfType(analyzedElement, PsiComment::class.java)
+                .forEach { comment->
+                    if(comment.tokenType == END_OF_LINE_COMMENT){
+                        descriptors.add(
+                                NamedFoldingDescriptor(
+                                        comment.node,
+                                        TextRange(comment.textRange.startOffset, comment.textRange.endOffset),
+                                        foldingGroup,
+                                        "${comment.text} highlighted"
+                                )
+                        )
+                    }
+                }
+
+        return descriptors.toTypedArray()
+    }
 
 
     override fun isCollapsedByDefault(node: ASTNode) = true
