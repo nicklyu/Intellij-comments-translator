ceef0d76e38b4589b5ecb29f1561f3f8d331afec
nickly <lyubimov.nkt@gmail.com>
2018-09-27 01:37:31 +0300

    Settings: invalid key exception handle

--- src/main/kotlin/com/nicklyu/translator/translators/YandexTranslator.kt
+++ src/main/kotlin/com/nicklyu/translator/translators/YandexTranslator.kt
@@ -1,8 +1,9 @@
 package com.nicklyu.translator.translators
 
 import com.intellij.openapi.project.Project
-import com.nicklyu.translator.ConfigReader
+import com.nicklyu.translator.settings.CommentsTranslatorSettingsState
 import com.nicklyu.translator.translators.caching.TranslationCacheManager
+import com.nicklyu.translator.translators.exceptions.InvalidApiKeyException
 import com.nicklyu.translator.translators.models.YandexResponse
 import com.nicklyu.translator.translators.models.get
 import okhttp3.MediaType
@@ -13,12 +14,13 @@ import org.slf4j.LoggerFactory
 
 class YandexTranslator(project: Project) : Translator {
     private val logger = LoggerFactory.getLogger(this::class.java)
+    private val settings = CommentsTranslatorSettingsState.instance
 
     private val commonUrlPart = "https://translate.yandex.net/api/v1.5/tr.json/translate"
     private val cacheManager: TranslationCacheManager = project.getComponent(TranslationCacheManager::class.java)
 
 
-    private val key = ConfigReader.readProperty("translator.yandex.key")
+    private val key = settings.yandexApiKey
     private val client = OkHttpClient()
 
     override fun translate(phrase: String): String {
@@ -36,7 +38,8 @@ class YandexTranslator(project: Project) : Translator {
                 .build()
         val response = client.newCall(request).execute().body()?.get<YandexResponse>()
         logger.debug("Translation status ${response?.code}. [${response?.text.orEmpty()}]")
-
+        if (response?.code == 401)
+            throw InvalidApiKeyException(response.message.orEmpty())
         val translation = response?.text?.joinToString().orEmpty()
         if (translation.isNotBlank())
             cacheManager.put(phrase, translation)
